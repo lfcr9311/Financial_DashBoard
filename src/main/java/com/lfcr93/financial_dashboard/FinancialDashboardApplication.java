e92a63672a37473d0b95b6a92835c23622294ff6
lfcr9311 <lfcr93@gmail.com>
2024-04-01 01:26:36 -0300

    iniciando repo

---
+++ src/main/java/com/lfcr93/financial_dashboard/FinancialDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.lfcr93.financial_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialDashboardApplication.class, args);
+	}
+
+}
